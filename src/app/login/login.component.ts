a54b24aa69c4009d3ad65c9d5dbfc3cb05224dde
= <=>
2024-04-15 14:39:32 +0200

    adds way to extract values from loin page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  email : string = "";
+  password : string = "";
+
+  onLogIn() {
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
