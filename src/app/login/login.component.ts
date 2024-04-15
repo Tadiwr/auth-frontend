1743094962af9e72652dea50a2d6b9921a6e1203
= <=>
2024-04-15 13:36:43 +0200

    adds code to extract the email and password from a login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, NgForm, FormsModule } from '@angular/forms';
+import { LoginCreds } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  onSubmit(form  : NgForm) {
+    // event.preventDefault();
+    console.log("Form is working baby");
+    const creds = form.value as LoginCreds;
+    console.log(creds);
+
+  }
 
 }
