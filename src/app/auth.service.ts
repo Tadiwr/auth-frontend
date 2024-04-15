37e40806917b1c96c1944b1e038bd5e2d1493c4a
= <=>
2024-04-15 15:59:24 +0200

    adds auyth service file

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'any',
 })
 export class AuthService {
 
-  constructor() { }
+  private base_url = "http://localhost:8080/auth";
+
+  constructor(private http : HttpClient, private cookies : CookieService) { }
+
+  login(creds : LoginCreds) {
+    const req_url = `${this.base_url}/login`;
+
+    return this.http.post<AuthRes>(req_url, creds, {
+      headers : {
+        "Accept" : "application/json",
+        "Content-Type" : "application/json"
+      }
+    })
+  }
+
+  public setAuthToken(token : string) {
+    this.cookies.set("auth_token", token);
+  }
+
+  public getAuthToken() : string {
+    return this.cookies.get("auth_token");
+  }
+
+  public clearAuthToken() {
+    this.cookies.delete("auth_token");
+  }
+
+}
+
+export type LoginCreds = {
+  email : string,
+  password : string
+}
+
+export type AuthRes = {
+  token : string,
+  message : string
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { Component} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { catchError, map } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -12,9 +16,39 @@ export class LoginComponent {
 
   email : string = "";
   password : string = "";
+  message : string = "Login";
 
-  onLogIn() {
+  constructor (private auth : AuthService, private router : Router) {}
+
+  async onLogIn() {
+
+    this.message = "Please wait...";
+
     console.log(this.email);
     console.log(this.password);
+
+    const creds = { email: this.email, password: this.password };
+    const req_url = "http://localhost:8080/auth/login";
+
+    this.auth.login(creds).pipe(
+      map((res) => {
+        this.message = res.message;
+        this.auth.setAuthToken(res.token);
+        this.router.navigate(["/user"]);
+
+      }),
+
+      catchError((err: HttpErrorResponse) => {
+
+        if (err.status === 404) {
+          this.message = "Account not found";
+        } else {
+          this.message = "Something went wrong...";
+        }
+
+        return this.message;
+
+      })
+    ).subscribe();
   }
 }
